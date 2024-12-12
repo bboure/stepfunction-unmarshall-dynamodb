f0a65d2cf538f690b73f4aa7bef7c5b5169d0765
bboure <benoit.boure@gmail.com>
2024-12-12 21:02:49 +0100

    Initial Commit

---
+++ lib/constructs/DynamoUnmarshall.ts
@@ -0,0 +1,49 @@
+import { CustomState } from "aws-cdk-lib/aws-stepfunctions";
+import { Construct } from "constructs";
+
+interface DynamoUnmarshallProps {
+  path: string;
+  variableName: string;
+}
+
+const generateUnmarshall = (path: string) => `{% (
+  $unmarshall := function ($object) {(
+      $type($object) = 'array' ? $map($object, $unmarshall)
+      : $merge($each($object, function ($val, $key) {
+            { $key: $convertValue($val) }
+        })
+      );
+  )};
+  
+  $convertValue := function ($object) {(
+    $type := $keys($object)[0];
+    $value := $lookup($object, $type);
+  
+    $type in ['S', 'SS', 'B', 'BS'] ?  $value
+        : $type in ['N'] ? $number($value)
+        : $type in ['M'] ? $unmarshall($value)
+        : $type in ['BOOL'] ? $value = 'true' or $value = true
+        : $type in ['L'] ? $map($value, $convertValue)
+        : $type in ['NS'] ? $value.$number()
+        : $type in ['NULL'] ? null
+        : $error('Unsupported type: ' & $type);
+  )};
+  
+  $unmarshall(${path});
+  ) %}`;
+
+export class DynamoUnmarshall extends CustomState {
+  constructor(scope: Construct, id: string, props: DynamoUnmarshallProps) {
+    const { path, variableName } = props;
+
+    super(scope, id, {
+      stateJson: {
+        Type: "Pass",
+        QueryLanguage: "JSONata",
+        Assign: {
+          [variableName]: generateUnmarshall(path),
+        },
+      },
+    });
+  }
+}
--- lib/step-function-unmarshall-dynamodb-stack.ts
+++ lib/step-function-unmarshall-dynamodb-stack.ts
@@ -1,16 +1,61 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
+import {
+  Chain,
+  CustomState,
+  DefinitionBody,
+  JsonPath,
+  StateMachine,
+} from "aws-cdk-lib/aws-stepfunctions";
+import {
+  DynamoAttributeValue,
+  DynamoGetItem,
+} from "aws-cdk-lib/aws-stepfunctions-tasks";
+import { Construct } from "constructs";
+import { DynamoUnmarshall } from "./constructs/DynamoUnmarshall";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class StepFunctionUnmarshallDynamodbStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const table = new Table(this, "Table", {
+      partitionKey: { name: "id", type: AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      billingMode: BillingMode.PAY_PER_REQUEST,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'StepFunctionUnmarshallDynamodbQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const dynamoDB = new DynamoGetItem(this, "DynamoDB", {
+      table,
+      key: {
+        id: DynamoAttributeValue.fromString(JsonPath.stringAt("$.id")),
+      },
+      resultSelector: {
+        item: JsonPath.stringAt("$.Item"),
+      },
+    });
+
+    const unmarshall = new DynamoUnmarshall(this, "Unmarshall", {
+      path: "$states.input.item",
+      variableName: "unmarshalledItem",
+    });
+
+    const combine = new CustomState(this, "Combine", {
+      stateJson: {
+        Type: "Pass",
+        QueryLanguage: "JSONata",
+        Output: {
+          unmarshalledItem: "{% $unmarshalledItem %}",
+          map: "{% $unmarshalledItem.MapAttribute.NestedString %}",
+          number: "{% $unmarshalledItem.NumberAttribute %}",
+        },
+      },
+    });
+
+    const chain = Chain.start(dynamoDB).next(unmarshall).next(combine);
+
+    const stateMachine = new StateMachine(this, "StateMachine", {
+      definitionBody: DefinitionBody.fromChainable(chain),
+    });
   }
 }
